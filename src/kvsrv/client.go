b5997462cf15cb7658e572cd1f82dbf9b84343da
Jason Reed <jreed0719@gmail.com>
2024-01-26 02:23:46 -0500

    Add working version of lab 2 key value server

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,13 +1,17 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
 	// You will have to modify this struct.
+	clientId       int64
+	requestCounter int
 }
 
 func nrand() int64 {
@@ -20,6 +24,8 @@ func nrand() int64 {
 func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.server = server
+	ck.clientId = nrand()
+	ck.requestCounter = 0
 	// You'll have to add code here.
 	return ck
 }
@@ -37,7 +43,16 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	args := GetArgs{key}
+	var reply GetReply
+
+	for {
+		ok := ck.server.Call("KVServer.Get", &args, &reply)
+
+		if ok {
+			return reply.Value
+		}
+	}
 }
 
 // shared by Put and Append.
@@ -50,7 +65,18 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
-	return ""
+	ck.requestCounter += 1
+
+	args := PutAppendArgs{key, value, ck.clientId, ck.requestCounter}
+	reply := PutAppendReply{}
+
+	for {
+		ok := ck.server.Call("KVServer."+op, &args, &reply)
+
+		if ok {
+			return reply.Value
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,104 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	kVMap        map[string]string
+	prevRequests map[int64]requestHistoryEntry
 }
 
+type requestHistoryEntry struct {
+	requestCounter int
+	value          string
+}
+
+func (kv *KVServer) checkDuplicate(clientId int64, requestCounter int) (string, bool) {
+	// Your code here.
+	prevEntry, ok := kv.prevRequests[clientId]
+
+	if !ok || prevEntry.requestCounter != requestCounter {
+		return "", false
+	}
+
+	return prevEntry.value, true
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	val, ok := kv.kVMap[args.Key]
+
+	if !ok {
+		val = ""
+	}
+
+	reply.Value = val
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	clientId := args.ClientId
+	requestCounter := args.RequestCounter
+	prevVal, isDuplicate := kv.checkDuplicate(clientId, requestCounter)
+
+	if isDuplicate {
+		reply.Value = prevVal
+		return
+	}
+
+	value := args.Value
+
+	kv.kVMap[args.Key] = value
+
+	kv.prevRequests[clientId] = requestHistoryEntry{requestCounter, ""}
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	clientId := args.ClientId
+	requestCounter := args.RequestCounter
+	prevVal, isDuplicate := kv.checkDuplicate(clientId, requestCounter)
+
+	if isDuplicate {
+		reply.Value = prevVal
+		return
+	}
+
+	key := args.Key
+	appendVal := args.Value
+
+	currVal, ok := kv.kVMap[key]
+
+	var newVal string
+	if ok {
+		newVal = currVal + appendVal
+		reply.Value = currVal
+	} else {
+		newVal = appendVal
+		currVal = ""
+		reply.Value = currVal
+	}
+
+	kv.kVMap[key] = newVal
+
+	kv.prevRequests[clientId] = requestHistoryEntry{requestCounter, currVal}
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
-	// You may need initialization code here.
+	kv.kVMap = make(map[string]string)
+	kv.prevRequests = make(map[int64]requestHistoryEntry)
 
 	return kv
 }
